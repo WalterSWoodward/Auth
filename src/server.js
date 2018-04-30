6c15ac07ad4d95c9c5ae3f3c1718f5033e9940b9
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-04-29 20:57:17 -0400

    complete for now
    used another student's work to comment an consider
    added comments where I felt the need

--- src/server.js
+++ src/server.js
@@ -1,119 +1,131 @@
-// Ryan, I believe, might have used create-react-app...or something, but at any
-// rate, you need to use app.js as your server.
-
-// const bodyParser = require('body-parser');
-// const express = require('express');
-// const session = require('express-session');
-
-// const STATUS_USER_ERROR = 422;
-// const BCRYPT_COST = 11;
-
-// const server = express();
-// // to enable parsing of json bodies for post requests
-// server.use(bodyParser.json());
-// server.use(session({
-//   secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
-// }));
-
-// /* Sends the given err, a string or an object, to the client. Sets the status
-//  * code appropriately. */
-// const sendUserError = (err, res) => {
-//   res.status(STATUS_USER_ERROR);
-//   if (err && err.message) {
-//     res.json({ message: err.message, stack: err.stack });
-//   } else {
-//     res.json({ error: err });
-//   }
-// };
-
-// // TODO: implement routes
-
-// // TODO: add local middleware to this route to ensure the user is logged in
-// server.get('/me', (req, res) => {
-//   // Do NOT modify this route handler in any way.
-//   res.json(req.user);
-// });
-
-// module.exports = { server };
-
+const bodyParser = require('body-parser');
 const express = require('express');
-const mongoose = require('mongoose');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
+const User = require('./user.js');
 
-const User = require('./auth/UserModel');
-
-mongoose
-  .connect('mongodb://localhost/authdb')
-  .then(() => {
-    // eslint-disable-next-line no-console
-    console.log('\n=== connected to MongoDB ===\n');
-  })
-  // eslint-disable-next-line no-console
-  .catch(err => console.log('database connection failed', err));
+const STATUS_USER_ERROR = 422;
+const BCRYPT_COST = 11;
 
 const server = express();
-
-// Custum Middleware
-// eslint-disable-next-line no-undef, func-names
-const authenticate = function (name) {
-  // eslint-disable-next-line func-names
-  return function (req, res, next) {
-    req.hello = `hello ${name}!`;
-    next();
-  };
-};
-
-server.use(express.json());
-
-// By default this is going to place something called session inside my requests
+server.use(bodyParser.json());
 server.use(
   session({
-    secret: 'you shall not pass!!',
+    // ???
+    name: 'auth',
+    secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+    resave: true,
+    saveUninitialized: false,
+    // ???
     cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 },
-    secure: false,
-    name: 'auth'
+    // ???
+    secure: false
   })
 );
 
-server.post('/login', (req, res) => {
+// If there's any error, or if the credentials are invalid, respond with an appropriate status and error
+// message using the `sendUserError()` helper function.
+const sendUserError = (err, res) => {
+  res.status(STATUS_USER_ERROR);
+  if (err && err.message) {
+    res.json({ message: err.message, stack: err.stack });
+  } else {
+    res.json({ error: err });
+  }
+};
+
+// STRETCH: write a piece of **global** middleware that
+// ensures a user is logged in when accessing _any_ route prefixed by
+// `/restricted/`.
+const restricted = (req, res, next) => {
+  const path = req.path;
+  if (/restricted/.test(path)) {
+    if (!req.session.loggedIn) {
+      sendUserError('You are not authorized to access this path', res);
+      return;
+    }
+  }
+  next();
+};
+
+server.use(restricted);
+
+// Route Handlers:
+// Play with this using Postman.  First post a valid user with a username, and password.
+// Then [POST] login with that users same validation info.  Then you should be able to see your user's information with the hashed password
+// at [GET] /me. Also you should be able to access [GET] /restricted
+
+server.post('/users', (req, res) => {
   const { username, password } = req.body;
-  User.findOne({ username })
-    .then((user) => {
-      if (user) {
-        // eslint-disable-next-line no-undef
-        user.isPasswordValid(password, cb); // maybe a promise
-      }
-    })
-    .catch(err => res.status(500).json(err));
-});
-
-server.get('/', (req, res) => {
-  req.session.name = 'Carlos';
-  res.send('have a cookie');
-  User.find().then(users => res.json(users));
-});
-
-server.get('/greet', (req, res) => {
-  const { name } = req.session;
-  res.send(`hello ${name}`);
-});
-
-// The idea here is when you have your login system, and you want to create
-// a new user, how do you achieve that? With this code here:
-// Remember that 'User' here is NOT the schema!  It is the model
-server.post('/register', (req, res) => {
-  const user = new User(req.body);
-
-  // this is a mongoose document that is mapped to a db document
+  if (!username || !password) {
+    sendUserError(
+      'You must provide a valid username and password to sign up',
+      res
+    );
+  }
+  const passwordHash = password;
+  const user = new User({ username, passwordHash });
   user
-    // another pre-packaged method???
     .save()
-    .then(savedUser => res.status(200).json(savedUser))
-    .catch(err => res.status(500).json(err));
+    .then((newUser) => {
+      res.status(200).json(newUser);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: 'There was a server error while signing up', err });
+    });
 });
 
-// eslint-disable-next-line no-console
-server.listen(5000, () => console.log('\n=== api on port 5000 ===\n'));
+server.post('/login', (req, res) => {
+  let { username } = req.body;
+  const { password } = req.body;
+  if (!username || !password) {
+    sendUserError('You must provide a username and password to sign in', res);
+    return;
+  }
+  username = username.toLowerCase();
+  User.findOne({ username }).exec((err, found) => {
+    if (err || found === null) {
+      sendUserError('No user found for that ID', res);
+      return;
+    }
+    bcrypt.compare(password, found.passwordHash, (error, verified) => {
+      if (error) {
+        res
+          .status(500)
+          .json({ error: 'There was in internal error while logging in' });
+      } else if (verified) {
+        req.session.loggedIn = found.id;
+        res.status(200).json({ success: true });
+      } else sendUserError('The password you entered is invalid', res);
+    });
+  });
+});
 
-// Schema - compiles -> model - new/instantiate -> mongoose document ->
+server.get('/restricted', (req, res) => {
+  res.status(200).json({ message: 'You have accessed the restricted content' });
+});
 
+const authenticate = (req, res, next) => {
+  if (req.session.loggedIn) {
+    User.findOne({ _id: req.session.loggedIn })
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ error: 'There was an internal error while processing' });
+      });
+  } else {
+    sendUserError('You must be logged in to the system', res);
+  }
+};
+
+server.get('/me', authenticate, (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = { server };
